41faa7da316ba889201ae41c44cc2adaac56e519
Takahiro Hara <takahiro.hb@gmail.com>
2021-08-28 18:07:04 +0900

    add 3.3 step7-10

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -17,7 +17,7 @@ class LessonController < ApplicationController
     render plain: "step4に移動しました。"
   end
 
-  ## フラッシュの実験
+  # フラッシュの実験
   def step5
     flash[:notice] = "step6に移動します"
     redirect_to action: "step6"
@@ -28,5 +28,27 @@ class LessonController < ApplicationController
 
   def step6
     render plain: flash[:notice]
+  end
+
+  # 3.3 テンプレートでインスタンス変数を使う実験
+  def step7
+    @price = (2000 * 1.08).floor
+    # 普通にテンプレートを使うときはrenderメソッドは不要
+  end
+
+  # 別のアクションのテンプレートを使う
+  def step8
+    @price = 1000
+    render "step7"
+  end
+
+  # HTMLの特殊文字は&lt;などに変換される。
+  # -> 検証モードで見てみたが変換されていなかった。ただ、スクリプトは実行されなかった。）
+  def step9
+    @comment = "<script>alert('危険!')</script>こんにちは。"
+  end
+
+  def step10
+    @comment = "<strong>安全なHTML</strong>"
   end
 end
