7161b060491dc60830a359fa17c3413e00d7df72
Takahiro Hara <takahiro.hb@gmail.com>
2021-09-07 01:53:09 +0900

    finished chapter 12

---
+++ app/assets/javascripts/articles.js
@@ -0,0 +1,13 @@
+$(document).on("turbolinks:load", function () {
+  var cb = $("#article_no_expiration");
+  var field = $("#article_expired_at");
+
+  var changeExpiredAt = function () {
+    if (cb.prop("checked")) field.hide();
+    else field.show();
+  };
+
+  cb.bind("click", changeExpiredAt);
+
+  changeExpiredAt();
+});
